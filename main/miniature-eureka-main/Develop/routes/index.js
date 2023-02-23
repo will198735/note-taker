b82a07b242d1dad53735b7130609eb8009641e3d
will198735 <recinoswilfredo@gmail.com>
2023-02-23 01:13:17 -0800

    modified server.js and deleted some useless files

--- main/miniature-eureka-main/Develop/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const tipsRouter = require('./notes');
-
-const app = express();
-
-app.use('/notes',);
--- main/miniature-eureka-main/Develop/server.js
+++ main/miniature-eureka-main/Develop/server.js
@@ -31,6 +31,30 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
+
+  //-----------------------------------------------------------------------
+
+
+
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for note`);
+  
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+  
+  app.post('/api/notes', (req, res) => {
+    // Let the client know that their POST request was received
+    res.json(`${req.method} request received`);
+  
+    // Show the user agent information in the terminal
+    console.info(req.rawHeaders);
+  
+    // Log our request to the terminal
+    console.info(`${req.method} request received`);
+  });
+  //-----------------------------------------------------------------------
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
