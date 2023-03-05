b1bd08c3698f4ab18512b502a46540a5e4900167
will198735 <recinoswilfredo@gmail.com>
2023-03-04 23:58:52 -0800

    modified server file added functions

--- main/miniature-eureka-main/Develop/server.js
+++ main/miniature-eureka-main/Develop/server.js
@@ -1,13 +1,18 @@
 
 const express = require('express');
+
 const path = require('path');
+const fs = require('fs');
+
 const db = require('./db/db.json');
-// const notesRoute = require('./routes/index.js');
+
+const uuid = require('./helpers/uuid');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Middleware for parsing JSON and urlencoded form data
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -17,7 +22,7 @@ app.use(express.static('public'));
 
 
 
-// GET Route for homepage
+
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -27,26 +32,33 @@ app.get('/notes', (req, res) =>
 );
 
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
 
 
 
+  app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    res.sendFile(path.join(__dirname,"db/db.json"))
+   
+    });
+  
+    app.post('/api/notes', (req, res) => {
 
-
-
-app.get('/', (req, res) => {
-  console.info(`${req.method} request received for note`);
-
-
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-});
-
-
-
-
-app.listen(PORT, () =>
+    console.info(`${req.method} request received to add a notes`);
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"))
+    const newNotes = req.body;
+    newNotes.id = uuid();
+    notes.push(newNotes);
+    fs.writeFileSync( "./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+    
+  });
+  app.delete('/api/notes/:id', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    const deleteNotes = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync('./db/db.json', JSON.stringify(deleteNotes));
+    res.json(deleteNotes);
+  })
+  app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
 
